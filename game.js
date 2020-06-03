4ba8b6e4cf1cdd12efbd430e1810325ddd517ed4
Leonardo Gonçalves <leomcg@gmail.com>
2020-06-03 12:08:25 -0300

    created snake.js and main game function

---
+++ game.js
@@ -0,0 +1,29 @@
+import { 
+  update as updateSnake, 
+  draw as drawSnake, 
+  SNAKE_SPEED 
+} from './snake.js'
+
+let lastRenderTime = 0;
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  
+  console.log('render');
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+
+}
+
+function draw() {
+
+}
